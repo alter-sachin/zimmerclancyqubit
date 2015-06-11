60b4f49198e949d1c0fb8c1121c689bebc9d9663
Aman <qubit93@outlook.com>
2015-06-12 03:08:19 +0530

    big fukin changes

--- runScript.R
+++ runScript.R
@@ -1,25 +1,51 @@
+
+cost <- function(theta)
+{
+  source("sigmoid.R")
+  m <- nrow(X)
+  g <- sigmoid(X%*%theta)
+  J <- (1/m)*sum((-y*log(g)) - ((1-y)*log(1-g)))
+  return(J)
+}
+
 runScript <- function(path)
 {
-  path<- "/home/qubit93/aman/wd_r/getting_cleaning_data/digits";
-  setwd(trainPath);
+  path<- "/home/qubit93/aman/wd_r/getting_cleaning_data/github_digits/zimmerclancyqubit";
+  setwd(path);
   
   #loading the dataset on memory in the form of a table
-  featureTrainData <- read.csv("train.csv",header = TRUE)
-  cols <- ncol(featureTrainData)
-  ##testing - featureTrainData[1:12,1:2]
-  XTrain <- featureTrainData[,2:cols]
-  yTrain <- featureTrainData[,1]
+  trainPath <- paste(path,"/digits",sep="")
+  setwd(trainPath)
+  dataDF <- read.csv("train.csv",header = TRUE)
+  setwd(path)
+  cols <- ncol(dataDF)
+  ##testing - dataDF[1:12,1:2]
+  XTrain <- dataDF[,2:cols]
+  yTrain <- dataDF$label
   
   dimensionsX <- dim(XTrain)
   m <- dimensionsX[1]
   n <- dimensionsX[2]
   
   bias <- rep(1,m)
+  XTrain <- cbind(bias,XTrain)
   
-  XTrain$new.col <- bias
+  #converting to matrices
+  X <- as.matrix(XTrain)
+  y <- as.matrix(yTrain)
   
+  #source("cost.R")
+  #source("sigmoid.R")
   ##initial parameters
   initialTheta <- rep(0,n+1)
+  cost(initialTheta)
   
-  
+  # running optimization algo
+  thetaOptim <- optim(par=initialTheta,fn=cost)
+  #setting theta
+  theta <- thetaOptim$par
+  #cost at optimal value of the theta
+  thetaOptim$value
 }
+
+runScript("")
