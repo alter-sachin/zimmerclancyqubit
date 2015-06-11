3f74738196b2f6b7a085d55188aca72e5f980d0d
Aman <qubit93@outlook.com>
2015-06-12 01:21:52 +0530

    New files

---
+++ runScript.R
@@ -0,0 +1,9 @@
+runScript <- function(path)
+{
+  path<- "/home/qubit93/aman/wd_r/getting_cleaning_data/digits";
+  setwd(trainPath);
+  
+  #loading the dataset on memory in the form of a table
+  featureTrainData <- read.csv("train.csv",header = TRUE)
+  
+}
