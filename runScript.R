5713fdd74f6b37768eb4b8f7937ca7808cf6afcb
Aman <qubit93@outlook.com>
2015-06-12 01:48:38 +0530

    changes made

--- runScript.R
+++ runScript.R
@@ -5,5 +5,21 @@ runScript <- function(path)
   
   #loading the dataset on memory in the form of a table
   featureTrainData <- read.csv("train.csv",header = TRUE)
+  cols <- ncol(featureTrainData)
+  ##testing - featureTrainData[1:12,1:2]
+  XTrain <- featureTrainData[,2:cols]
+  yTrain <- featureTrainData[,1]
+  
+  dimensionsX <- dim(XTrain)
+  m <- dimensionsX[1]
+  n <- dimensionsX[2]
+  
+  bias <- rep(1,m)
+  
+  XTrain$new.col <- bias
+  
+  ##initial parameters
+  initialTheta <- rep(0,n+1)
+  
   
 }
